bac5d51551aaa58cd43b0c12517657477a072fe4
Iliyan Stoyanov <virus_22@abv.bg>
2018-01-06 19:30:14 +0200

    Integrated ASP Identity :)

---
+++ Jcars/Jcars/App_Start/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Jcars.Business.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jcars.App_Start
+{
+    public class Startup
+    {
+        public static Func<UserManager<User>> UserManagerFactory { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/auth/login")
+            });
+
+            // configure the user manager
+            UserManagerFactory = () =>
+            {
+                var usermanager = new UserManager<User>(
+                    new UserStore<User>(new JcarsDbContext()));
+                // allow alphanumeric characters in username
+                usermanager.UserValidator = new UserValidator<User>(usermanager)
+                {
+                    AllowOnlyAlphanumericUserNames = false
+                };
+
+                return usermanager;
+            };
+        }
+
+    }
+}
